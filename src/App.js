0629aa2b60a6ed5948cedbf1e7800d55db6b7301
Uttkarsh Narayan <uttkarshnarayan@Uttkarshs-MacBook-Pro.local>
2022-04-26 19:16:41 -0700

    app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import React, { useState , component} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./puzzle.css";
 import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
 import img from "./UnessayPuzzle_color.png";
 
-import { HashRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Homepage";
-import Page2 from "./components/Page2";
-import "./App.css";
-
-
-class App extends Component {
-render() {
-  const [text, setText] = useState("Unpuzzle the pieces!!");
+function App() {
+	const [text, setText] = useState("Unpuzzle the pieces!!");
 	
 	const set = () => {
 		setText("Congratulations!!");
 	};
-  return (
-    <div>
-      <HashRouter basename={process.env.PUBLIC_URL}>
-        <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/page2" component={Page2} />
-        </Switch>
-        </div>
-      </HashRouter>
+	
+	return (
+		<>
 			<h2 className="tag">{text}</h2>
 			<JigsawPuzzle
 				imageSrc={img}
@@ -36,9 +22,9 @@ render() {
 				onSolved={set}
 				className="jigsaw-puzzle"
 			/>
-    </div>
-    );
-  }
+		</>
+	);
 }
+
 export default App;
 
