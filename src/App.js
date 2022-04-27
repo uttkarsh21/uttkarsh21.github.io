e5c13a1f806a1c17d432b72270ca1bdd8b81083f
Uttkarsh Narayan <uttkarshnarayan@Uttkarshs-MacBook-Pro.local>
2022-04-26 22:35:54 -0700

    puzzle complete

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./puzzle.css";
 import { JigsawPuzzle } from "react-jigsaw-puzzle/lib";
 import "react-jigsaw-puzzle/lib/jigsaw-puzzle.css";
 import img from "./UnessayPuzzle_color.png";
+import frontImg from "./UnessayPuzzle_DirectionsFront.jpg";
+import backImg from "./UnessayPuzzle_Directions_Back.jpg";
 
 function App() {
-	const [text, setText] = useState("Unpuzzle the pieces!!");
+	const [text, setText] = useState("Unessay Connection - Connect all the pieces!!");
 	
 	const set = () => {
 		setText("Congratulations!!");
@@ -14,11 +16,15 @@ function App() {
 	
 	return (
 		<>
+      <div className="instruction-container">      
+        <img src={frontImg} className="instructions-front" alt="front" />
+        <img src={backImg} className="instructions-back" alt="front" />
+      </div>
 			<h2 className="tag">{text}</h2>
 			<JigsawPuzzle
 				imageSrc={img}
-				rows={5}
-				columns={5}
+				rows={3}
+				columns={3}
 				onSolved={set}
 				className="jigsaw-puzzle"
 			/>
